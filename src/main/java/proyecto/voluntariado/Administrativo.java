d00e1c139715c45e0d348b782a91fefa8380d493
fefex <fefex@192.168.1.86>
2024-10-06 20:51:29 -0300

    se agrego la clase Administrativo

---
+++ src/main/java/proyecto/voluntariado/Administrativo.java
@@ -0,0 +1,20 @@
+package proyecto.voluntariado;
+
+public class Administrativo extends Personas {
+    private String area;
+
+    public Administrativo(String nombre, String primerApellido, String segundoApellido, String rut, String area) {
+        super(nombre, primerApellido, segundoApellido, rut);
+        this.area = area;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    @Override
+    public String getNombre() {
+        return "Administrativo: " + super.getNombre();
+    }
+}
+
