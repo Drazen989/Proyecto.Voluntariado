11486829ebde9062f52c758f4b304337a05c538e
drazen989 <drazen989@192.168.1.86>
2024-10-06 20:40:50 -0300

    se agrego la clase Informacion personal

---
+++ src/main/java/InformacionPersonal.java
@@ -0,0 +1,49 @@
+package proyecto.voluntariado;
+
+public class InformacionPersonal {
+    private String genero;
+    private String fechaNacimiento;
+    private String correo;
+    private int numero;
+    private String region;
+    private String comuna;
+
+    public InformacionPersonal(String genero, String fechaNacimiento, String correo, int numero) {
+        this.genero = genero;
+        this.fechaNacimiento = fechaNacimiento;
+        this.correo = correo;
+        this.numero = numero;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public String getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public String getComuna() {
+        return comuna;
+    }
+
+    public void setComuna(String comuna) {
+        this.comuna = comuna;
+    }
+}
